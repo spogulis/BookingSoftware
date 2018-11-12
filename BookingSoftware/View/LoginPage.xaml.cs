0913e586b28a019a553509b19b0573a0b1cc0cd2
Mészáros Hajnalka <hajn0020@edu.easj.dk>
2018-11-11 17:08:28 -0800

    [ADD] : LoginPage & ForgotPasswordPage

---
+++ BookingSoftware/View/LoginPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Windows.ApplicationModel.Core;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FranceVacanses.View
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class LoginPage : Page
+    {
+       
+        public List<string> Gender { get => this.gender; set => this.gender = value; }
+
+        List<string> gender;
+        public LoginPage()
+        {
+            this.InitializeComponent();
+
+            ApplicationViewTitleBar formattableTitleBar = ApplicationView.GetForCurrentView().TitleBar;
+            formattableTitleBar.ButtonBackgroundColor = Colors.Transparent;
+            CoreApplicationViewTitleBar coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+
+            gender = new List<string>{};
+            gender.Add("Male");
+            gender.Add("Female");
+
+         
+
+
+
+        }
+        private void ForgotPassword_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(ForgetPasswordPage));
+        }
+
+        private void ForgotPassword_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            
+        }
+    }
+}
