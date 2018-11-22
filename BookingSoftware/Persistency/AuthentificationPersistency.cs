c293c793a49682392646fb9f87605c45cbcbec22
Bartosz <DonMarchewa@gmail.com>
2018-11-22 14:50:13 +0100

    Added some stuff

--- BookingSoftware/Persistency/AuthentificationPersistency.cs
+++ BookingSoftware/Persistency/AuthentificationPersistency.cs
@@ -3,51 +3,162 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BookingSoftware.Model;
+using System.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+using Windows.Security.Cryptography;
+using Windows.Storage.Streams;
 using Newtonsoft.Json;
-using System.IO;
+using Windows.Storage;
+using System.Collections.ObjectModel;
 using System.Runtime.CompilerServices;
+using Windows.ApplicationModel.AppExtensions;
+using BookingSoftware.Model;
+using System.Windows;
+using Windows.UI.Popups;
 
 namespace BookingSoftware.Persistency
 {
-    class AuthentificationPersistency
+   static class AuthentificationPersistency
     {
-        //Model of customer.
-        public Customer _customer = ActiveUserSingleton.GetActiveUser() as Customer;
-        //We need dictionary to store data while program is running.
-        private Dictionary<int, object> _customers = new Dictionary<int, object>();
-        //ID which will increment after creating new account.
-        private int ID = 0;
+        public static StorageFolder folder = ApplicationData.Current.LocalFolder;
+        public static StorageFile Customers;
+        public static StorageFile logfile;
+        public static string TempStringCustomer;
 
         
-       
-       
+
+
         //Writing Dicionary to JSON and file.
-        public void Writing(string path, Dictionary<int,object> dictionary)
+        public static async void SavngCustomerToFile(List<Customer> list)
         {
-          using (StreamWriter file = File.CreateText(path))
+            string json = JsonConvert.SerializeObject(list, Formatting.Indented);
+            Customers = await folder.CreateFileAsync("Customer.json", CreationCollisionOption.ReplaceExisting);
+            await FileIO.WriteTextAsync(Customers, json);
+        }
+       
+        
+        //Getting Customers string from Json.
+        private static async Task<string> GetCustomersStrigFromJson()
+        {
+            Customers = await folder.CreateFileAsync("HostsList.json", CreationCollisionOption.OpenIfExists);
+            string json = await FileIO.ReadTextAsync(Customers);
+
+            if (json == null)
             {
-                JsonSerializer serializer = new JsonSerializer();
-                serializer.Serialize(file, dictionary);
+                TempStringCustomer = "";
+            }
+            else { TempStringCustomer = json; }
+            return TempStringCustomer;
+        }
+       
+        
+        //Creating log file to store there all exceptions.
+        public static async void AddLog(string log)
+        {
+
+            try
+            {
+                logfile = await folder.CreateFileAsync("Logfile.txt", CreationCollisionOption.OpenIfExists);
+                log = log + await FileIO.ReadTextAsync(logfile);
+                await FileIO.WriteTextAsync(logfile, log);
+            }
+            catch (Exception)
+            {
+
             }
         }
-        //Reading File to JSON and JSON to Dicionary.
-        public void Reading(string path)
+
+      
+        //Getting Customers list from File.
+        public static async Task<List<Customer>> GetCustomersListFromFile()
+        {
+
+            await GetCustomersStrigFromJson();
+            List<Customer> returnedlist = new List<Customer>();
+            try
             {
-            using (StreamReader file = File.OpenText(path))
-                {
-                JsonSerializer serializer = new JsonSerializer();
-                Dictionary<int,object> dictionary = (Dictionary<int,object>)serializer.Deserialize(file, typeof(Dictionary<int,object>));
-                }
+                returnedlist = JsonConvert.DeserializeObject<List<Customer>>(TempStringCustomer);
+
             }
+            catch (Exception e)
+            {
+                AuthentificationPersistency.AddLog($"{DateTime.Now}   exception {e.Message} was caught while getting Customers list\n");
 
+            }
+            if (returnedlist == null)
+            {
+                return new List<Customer>();
+            }
+            else return returnedlist;
 
+        }
+
+        
+        //Checking existence of user.
+        public static async Task<bool> CheckIfExists()
+        {
+            Customer _customer = ActiveUserSingleton.GetActiveUser() as Customer;
+            List<Customer> Customers = await GetCustomersListFromFile();
+            Customer customer = Customers.FirstOrDefault(x => x.Email == _customer.Email);
+            Customer customer3 = Customers.FirstOrDefault(x => x.PhoneNumber == _customer.PhoneNumber);
+            if (customer3 == null || customer == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static async Task<Customer> GetUserByEmail()
+        {
+            Customer returnedCusomer = new Customer();
+            Customer _customer = ActiveUserSingleton.GetActiveUser() as Customer;
+            List<Customer> clist = await GetCustomersListFromFile();
+            returnedCusomer = clist.FirstOrDefault(x => x.Email == _customer.Email);
+            return returnedCusomer;
+
+        }
         //Create Account
-        public void CreateAccount()
+        public static async void CreateAccount()
         {
-            _customers.Add(ID,_customer);
-            Writing(@"c:\Projekt\CustomerModel.json",_customers);
-            ID++;
+
+            if (await CheckIfExists())
+            {
+               var dialog = new MessageDialog("You need to use other Email or Phone Number.");
+                await dialog.ShowAsync();
+            }
+            else
+            {
+                Customer _customer = ActiveUserSingleton.GetActiveUser() as Customer;
+                List<Customer> clist = await GetCustomersListFromFile();
+                clist.Add(_customer);
+                SavngCustomerToFile(clist);
+            }
         }
+        //Modify account
+        //public static async void ModifyAccount()
+        //{
+        //    Customer _customer = ActiveUserSingleton.GetActiveUser() as Customer;
+        //    List<Customer> clist = await GetCustomersListFromFile();
+        //    int index = clist.FindIndex(x => x.Name == _customer.Name);
+        //    clist[index] =
+
+        //}
+        
+
+        //Login to account.
+        public static async void LoginToAcc()
+        {
+            if (await CheckIfExists())
+            {
+                var dialog = new MessageDialog("You need to use other Email or Phone Number.");
+                await dialog.ShowAsync();
+            }
+            else
+
+        }
+
     }
 }
