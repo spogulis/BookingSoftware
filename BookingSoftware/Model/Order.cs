a69183129ae86649a58ce5661b32537e28a22bcb
Bartosz <DonMarchewa@gmail.com>
2018-11-29 14:00:58 +0100

    Order model and OrdersPersistency class added

---
+++ BookingSoftware/Model/Order.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingSoftware.Model
+{
+    class Order
+    {
+        private string _facilityName;
+        private string _address;
+        private int _area;
+        private int _price;
+        private string _owner;
+        private int _phoneNumber;
+        private string _email;
+        private int _rating;
+
+
+        public string FacilityName
+        {
+            get { return _facilityName; }
+            set { _facilityName = value; }
+        }
+
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        public int Area
+        {
+            get { return _area; }
+            set { _area = value; }
+        }
+
+        public int Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        public string Owner
+        {
+            get { return _owner; }
+            set { _owner = value; }
+        }
+
+        public int PhoneNumber
+        {
+            get { return _phoneNumber; }
+            set { _phoneNumber = value; }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        public int Rating
+        {
+            get { return _rating; }
+            set { _rating = value; }
+        }
+
+
+
+
+
+    }
+}
+
---
+++ BookingSoftware/Persistency/OrdersPersistency.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using BookingSoftware.Model;
+using Newtonsoft.Json;
+
+namespace BookingSoftware.Persistency
+{
+    static class OrdersPersistency
+    {
+        public static StorageFolder folder = ApplicationData.Current.LocalFolder;
+
+        public static StorageFile Orders;
+
+        public static StorageFile OrdersLogFile;
+
+        public static string TempStringOrders;
+
+        private static Order _order = new Order();
+
+
+        public static async void SavingOrdersToFile(List<Order> list)
+        {
+            string json = JsonConvert.SerializeObject(list, Formatting.Indented);
+            Orders = await folder.CreateFileAsync("Orders.json", CreationCollisionOption.ReplaceExisting);
+            await FileIO.WriteTextAsync(Orders, json);
+        }
+
+
+        private static async Task<string> GetOrderStringFromJson()
+        {
+            Orders = await folder.CreateFileAsync("Order.json", CreationCollisionOption.OpenIfExists);
+            string json = await FileIO.ReadTextAsync(Orders);
+            if (json == null)
+            {
+                TempStringOrders = "";
+            }
+            else
+                TempStringOrders = json;
+            return TempStringOrders;
+        }
+
+
+        public static async void AddLog(string log)
+        {
+            try
+            {
+                OrdersLogFile = await folder.CreateFileAsync("Logfile.txt", CreationCollisionOption.OpenIfExists);
+                log = log + await FileIO.ReadTextAsync(OrdersLogFile);
+                await FileIO.WriteTextAsync(OrdersLogFile, log);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+
+        public static async Task<List<Order>> GetOrdersListFromFile()
+        {
+            await GetOrderStringFromJson();
+            List<Order> returnedlist = new List<Order>();
+            try
+            {
+                returnedlist = JsonConvert.DeserializeObject<List<Order>>(TempStringOrders);
+            }
+            catch (Exception e)
+            {
+                OrdersPersistency.AddLog($"{DateTime.Now} exception {e.Message} was caught while getting Orders list\n");
+            }
+
+            if (returnedlist == null)
+            {
+                return new List<Order>();
+            }
+            else return returnedlist;
+        }
+
+
+    }
+}
